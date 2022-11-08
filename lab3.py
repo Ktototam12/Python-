6f01dab3d56e1df9df4b4f3279ec7d4c4f27b7b3
Анна Кирпа <70509092+Ktototam12@users.noreply.github.com>
2022-11-08 17:54:24 +0500

    lab3

---
+++ lab3.py
@@ -0,0 +1,9 @@
+# cd "C:\Users\ktoto\OneDrive\Рабочий стол\Маг 1\python\lab3"
+# python3 -m venv ./venv
+# cd ./venv/Scripts/
+#.\Activate.Psl
+#cd ..
+#cd /
+# cd "C:\Users\ktoto\OneDrive\Рабочий стол\Маг 1\python\lab3"
+#pip install pandas
+#pip install flask
